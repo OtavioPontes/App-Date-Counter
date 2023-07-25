7c6c386c71b2a03ff2d93f8c8c56d62c2384b599
Otávio Pontes <otavio.pontes1103@gmail.com>
2023-07-25 16:24:46 -0300

    Initial Commit

---
+++ AppCounter/AppCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCounterApp.swift
+//  AppCounter
+//
+//  Created by Otávio Pontes on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AppCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCounter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppCounter
+//
+//  Created by Otávio Pontes on 25/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
